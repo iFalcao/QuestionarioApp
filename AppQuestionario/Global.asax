ec4ed55b8e626ece0084c5a3324e29845e09cd4d
Ícaro Falcão <vbsfalcao@gmail.com>
2017-07-06 10:12:00 -0300

    Inicia a aplicação

---
+++ AppQuestionario/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppQuestionario.Global" Language="C#" %>
---
+++ AppQuestionario/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppQuestionario.Startup))]
+namespace AppQuestionario
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
